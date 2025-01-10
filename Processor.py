e0eb1e4feda33dafbdf4421b1658c7d9dae5d74d
intersectingWaveFunction <scndthe2nd@github.com>
2025-01-10 09:40:17 -0500

    set to python and included sample

--- Processor.py
+++ Processor.py
@@ -1,11 +1,32 @@
 
 #### Process ####
 # Pull information from an excel sheet
-# Label information by filter
-# Create a template for each label
-# Add templates to a pdf queue
-# Create a pdf from that information
+## Get correct cell collumn
 
+# SAMPLE #
+import openpyxl
+
+wb = openpyxl.load_workbook(r'c:*specific destination of file*.xlsx')
+sheet = wb.active
+x1 = sheet['B3'].value
+x2 = sheet['B4'].value
+y1 = sheet['C3'].value
+y2 = sheet['C4'].value
+
+
+
+print(x1,x2,y1,y2)
+
+
+# Label information by filter
+
+# Create a template for each label
+## Define template
+# Add templates to a pdf queue
+## Create a pdf queue
+## add items in to pdf queue 
+# Create a pdf from that information
+## send to pdf printer
 
 #### Interface ####
 # Create user interface
